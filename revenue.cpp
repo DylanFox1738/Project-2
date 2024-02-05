4c411d1182f9f49469fd1fc58bb46427efa9d7e3
DylanFox1738 <157341926+DylanFox1738@users.noreply.github.com>
2024-02-04 21:07:29 -0800

    Update revenue.cpp
    I had a lot of trouble with the pushing updating from pwershell to github since i have only done this with someone helping me, so i just manually made changes here

--- revenue.cpp
+++ revenue.cpp
@@ -1,3 +1,26 @@
-int main() {
+#include <iostream>
 
+int main() {
+    // Declare variables
+    double b, r, c;
+    int N;
+
+    // Define base cost, revenue per sale and cost per make
+    std::cout << "Enter the amount of dollars for the company base cost: ";
+    std::cin >> b;
+
+    std::cout << "Enter how much money is made per device sale: ";
+    std::cin >> r;
+
+    std::cout << "Enter how much it costs to make each device. This number should be less than revenue per sale.: ";
+    std::cin >> c;
+
+    
+    // Equation for number of devices needed to be sold
+    N = b / (r - c);
+
+    // Result output
+    std::cout << "In order to have a non-negative net profit, aka break zero, the amount of devices we must sell is: " << N << std::endl;
+   
+    return 0;
 }
